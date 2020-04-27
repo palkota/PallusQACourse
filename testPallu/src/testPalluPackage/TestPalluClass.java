313883b00b76bc72e3e5a94b233de4b9ec39fb59
Dell <Dell@192.168.1.7>
2020-04-27 19:34:03 +0530

    testPallu_27April_2020

---
+++ testPallu/src/testPalluPackage/TestPalluClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package testPalluPackage;
+
+/**
+ * @author Dell
+ *
+ */
+public class TestPalluClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testPallu/src/testPalluPackage/modulus.java
@@ -0,0 +1,20 @@
+package testPalluPackage;
+
+public class modulus
+{
+
+	public static void main(String[] args)
+	{
+		int num=69;
+		int mod=0;
+		int quo=0;
+		
+		mod=num%10;
+		quo=num/10;
+		
+		System.out.println("modulus is" +mod);
+		System.out.print("quotient is"+quo);
+
+	}
+
+}
